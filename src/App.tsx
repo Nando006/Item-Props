b395046d34edd4189d930c0613c6dec16a7e53ff
Nando <fsnando.012@outlook.com>
2025-02-07 13:25:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import ItemsJSON from "./components/ItemsJSON";
+
+function App() {
+  return (
+    <>
+      <ItemsJSON />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemsJSON.tsx
@@ -0,0 +1,7 @@
+export default function ItemsJSON() {
+  return (
+    <div>
+      <aside></aside>
+    </div>
+  );
+}
